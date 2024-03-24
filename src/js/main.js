ac717fa4158ca06237632e4756a685bc3ea128f4
sergiorobla <sergiorobla36@gmail.com>
2024-03-24 22:57:20 +0100

    finish api

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnPapel = document.getElementById("papel");
     const btnTijera = document.getElementById("tijera");
     const resultadosDiv = document.getElementById("resultados");
+    const contadorDiv = document.getElementById("contador");
+    let contadorEmpate = 0;
+    let contadorVictoria = 0;
+    let contadorDerrota = 0;
     btnPiedra?.addEventListener("click", function () {
         jugar("✊");
     });
@@ -18,24 +22,25 @@ document.addEventListener("DOMContentLoaded", function () {
         const eleccionComputadora = opciones[Math.floor(Math.random() * opciones.length)];
         if (resultadosDiv) {
             resultadosDiv.innerHTML = "";
-            resultadosDiv.innerHTML += `<div class="resultadoMaquina">${eleccionComputadora}</div>`;
+            resultadosDiv.innerHTML += `<div class="resultadoMaquina">${eleccionUsuario} vs ${eleccionComputadora}</div>`;
             if (eleccionUsuario === eleccionComputadora) {
-                resultadosDiv.innerHTML +=
-                    `<div class="resultadoFinal">¡Buen Empate!</div>`;
+                resultadosDiv.innerHTML += `<div class="resultadoFinal">¡Buen Empate!</div>`;
+                contadorEmpate++;
             }
             else if ((eleccionUsuario === "✊" && eleccionComputadora === "✌") ||
                 (eleccionUsuario === "✋" && eleccionComputadora === "✊") ||
                 (eleccionUsuario === "✌" && eleccionComputadora === "✋")) {
-                resultadosDiv.innerHTML +=
-                    `<div class="resultadoFinal">Enhorabuena, <br> ¡Ganaste!</div>`;
+                resultadosDiv.innerHTML += `<div class="resultadoFinal">Enhorabuena, <br> ¡Ganaste!</div>`;
+                contadorVictoria++;
             }
             else {
-                resultadosDiv.innerHTML +=
-                    `<div class="resultadoFinal">Que lástima... <br> Perdiste</div>`;
+                resultadosDiv.innerHTML += `<div class="resultadoFinal">Que lástima... <br> Perdiste</div>`;
+                contadorDerrota++;
             }
         }
         else {
-            console.error("El elemento del reusltado es null.");
+            console.error("El elemento del resultado es null.");
         }
+        contadorDiv.innerHTML = `<div class="contadorResul">Victorias: ${contadorVictoria}, Derrotas: ${contadorDerrota}, Empates: ${contadorEmpate}</div>`;
     }
 });
